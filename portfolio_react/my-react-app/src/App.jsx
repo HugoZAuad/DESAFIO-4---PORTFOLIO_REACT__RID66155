f998fe4d49fb182e7de20233672dc6b2e8fbb52e
HugoZAuad <hugozeymer@hotmail.com>
2025-05-22 16:33:29 -0300

    news configs and pakcs

---
+++ portfolio_react/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Olá, Material-UI no React 19!</h1>
+      <Button variant="contained" color="primary">
+        Clique aqui
+      </Button>
+    </div>
+  );
+}
+
+export default App;
